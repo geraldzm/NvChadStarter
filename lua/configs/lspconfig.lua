7881b99845a43cb17e4cb605c9418518f950b803
gerald <geraldzmt@gmail.com>
2024-09-23 15:29:28 -0600

    working setup

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,10 +1,11 @@
+-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md 
 -- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls" }
+local servers = { "html", "cssls", "pylsp", "gopls", "ts_ls" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
@@ -16,9 +17,78 @@ for _, lsp in ipairs(servers) do
   }
 end
 
--- configuring single server, example: typescript
--- lspconfig.ts_ls.setup {
---   on_attach = nvlsp.on_attach,
---   on_init = nvlsp.on_init,
---   capabilities = nvlsp.capabilities,
--- }
+-- Typescript personalized config 
+lspconfig.ts_ls.setup {
+  on_attach = function(client, bufnr)
+    nvlsp.on_attach(client, bufnr)
+    -- Set up format on save if you want ts_ls to handle formatting
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = bufnr,
+      callback = function()
+        vim.lsp.buf.format({ timeout_ms = 2000 })
+      end,
+    })
+  end,
+  on_init = nvlsp.on_init,
+  capabilities = nvlsp.capabilities,
+  settings = {
+    typescript = {
+      preferences = {
+        importModuleSpecifier = "non-relative"
+      }
+   }
+  },
+  init_options = {
+    preferences = {
+      importModuleSpecifier = "non-relative"
+    }
+  }
+}
+
+-- golang personalized config
+lspconfig.gopls.setup {
+  on_attach = nvlsp.on_attach,
+  on_init = nvlsp.on_init,
+  capabilities = nvlsp.capabilities,
+  settings = {
+    gopls = {
+      completeUnimported = true,
+      usePlaceholders = true,
+      analyses = {
+        unusedparams = true,
+      },
+      staticcheck = true,
+      gofumpt = true,
+    },
+  },
+}
+
+-- python personalized config 
+lspconfig.pylsp.setup {
+  on_attach = function(client, bufnr)
+    nvlsp.on_attach(client, bufnr)
+    -- Set up format on save
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = bufnr,
+      callback = function()
+        vim.lsp.buf.format({ timeout_ms = 2000 })
+      end,
+    })
+  end,
+  on_init = nvlsp.on_init,
+  capabilities = nvlsp.capabilities,
+  settings = {
+    pylsp = {
+      plugins = {
+        pycodestyle = {
+          ignore = {'W391', 'E302'},
+          maxLineLength = 150
+        }
+      }
+    },
+    black = {
+      enabled = true,
+      line_length = 150,
+    },
+  }
+}
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,8 +1,26 @@
 return {
   {
     "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
+    event = 'BufWritePre', -- uncomment for format on save
     opts = require "configs.conform",
+  },
+
+  -- Language servers
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "stylua",
+        "html-lsp",
+        "css-lsp",
+        "prettier",
+        "pylsp",
+        "gopls",
+        "black",
+        "typescript-language-server",
+      },
+    },
   },
 
   -- These are some examples, uncomment them if you want to see them work!
@@ -11,15 +29,15 @@ return {
     config = function()
       require "configs.lspconfig"
     end,
-  },
+  }  ,
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+  	"nvim-treesitter/nvim-treesitter",
+  	opts = {
+  		ensure_installed = {
+  			"vim", "lua", "vimdoc",
+       "html", "css", "python", "golang", "typescript"
+  		},
+  	},
+  },
 }
