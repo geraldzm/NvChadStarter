7b9ccb5c5d4846fa92fa5381b0de87c267fb9ca9
gerald <geraldzmt@gmail.com>
2024-10-26 10:34:50 -0600

    Add surroundings

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -40,4 +40,25 @@ return {
   		},
   	},
   },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    config = function()
+      require'nvim-treesitter.configs'.setup {
+        textobjects = {
+          select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+              ["af"] = "@function.outer",
+              ["if"] = "@function.inner",
+              ["ac"] = "@class.outer",
+              ["ic"] = "@class.inner",
+            },
+          },
+          -- You can also add move and swap configurations here if desired
+        },
+      }
+    end,
+  },
 }
