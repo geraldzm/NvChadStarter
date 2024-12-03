f85c26749964f64c77eed1bd89f3f4125577cd84
gerald <geraldzmt@gmail.com>
2024-12-03 10:31:38 -0600

    Delete instances

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,8 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+-- Delete selected instances
+map("v", "<space>d", "y:%s#\\V<C-r>\"##g<CR>", { desc = "Delete all instances" })
+map("v", "<space>D", "y:%s#\\V<C-r>\"##gc<CR>", { desc = "Delete all instances with confirmation" })
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
