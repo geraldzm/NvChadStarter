95e5552419725e075e307ea416c205ae8ff0ac33
geraldzm <geraldzmt@gmail.com>
2024-12-04 11:07:39 -0600

    mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -10,5 +10,8 @@ map("i", "jk", "<ESC>")
 -- Delete selected instances
 map("v", "<space>d", "y:%s#\\V<C-r>\"##g<CR>", { desc = "Delete all instances" })
 map("v", "<space>D", "y:%s#\\V<C-r>\"##gc<CR>", { desc = "Delete all instances with confirmation" })
+-- Replace selected instances with custom text
+map("v", "<space>r", "y:%s#\\V<C-r>\"##g<left><left>", { desc = "Replace all instances (type replacement)" })
+map("v", "<space>R", "y:%s#\\V<C-r>\"##gc<left><left>", { desc = "Replace all instances (type replacement)" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
